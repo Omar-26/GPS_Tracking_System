2151790b48a337e5f25a5b299079e23779702618
Omar Ashraf <eng.omar.ashraf26@gmail.com>
2024-04-24 17:20:31 +0200

    Updated COTS

---
+++ COTS/01_MCAL/UART/UART_Program.c
@@ -0,0 +1,15 @@
+/**************************************************************/
+/********** Name      : TEAM_NO	        	        ***********/
+/********** Date      : 24/04/2024              	***********/
+/********** File Name : UART_Program.c              ***********/
+/********** Version   : 1.0                    		***********/
+/**************************************************************/
+/**< LIB */
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "tm4c123gh6pm.h"
+/**< MCAL_UART */
+#include "UART_private.h"
+#include "UART_interface.h"
+#include "UART_config.h"
+/****************************************< UART_FUNCTIONS_IMPLEMENTATION ****************************************/
