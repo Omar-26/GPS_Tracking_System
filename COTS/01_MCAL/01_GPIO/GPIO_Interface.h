de92c71a6f2819a4130e746e6be13955b5d40664
ahmed <ahmedhamadi332@gmail.com>
2024-05-01 20:18:09 +0300

    added gpio files

--- COTS/01_MCAL/01_GPIO/GPIO_Interface.h
+++ COTS/01_MCAL/01_GPIO/GPIO_Interface.h
@@ -1,15 +1,22 @@
 #include<stdio.h>
 #include<d:\Projects\C_projects\tm4c123gh6pm.h>
-
+// led color
 #define RED_LED 0
 #define BLUE_LED 1
 #define GREEN_LED 2
+// switches
 #define SW_1 0
 #define SW_2 1
+// led state
 #define LED_OFF 0
 #define LED_ON 1
-#define portF 1
-#define portA 0
+// Gpio ports
+#define portA (0)
+#define portB (1)
+#define portC (2)
+#define portD (3)
+#define portE (4)
+#define portF (5)
 //GPIO pins
 #define pin0 (1<<0)
 #define pin1 (1<<1)
@@ -25,9 +32,21 @@
 //Gpio pin direction
 #define pin_out (1)
 #define pin_in  (0)
+// keys 
+#define key_lock 0x4c4f434B
+#define Enable_portF_pins 0x1F
+#define Enable_portA_Uart_Spi 0x3F
+#define PUL_R_PortF 0x11
+#define PUL_R_PortA 0x33
+#define Out_Leds 0x0E
+#define Out_TXs 0x22
+#define No_Alternate_func 0x00000000
+#define Uart_Spi_PortA 0x3F
+
+//function prototypes
 void init_port_f();
 void init_port_A();
-unsigned char Get_Switch(unsigned char sw);
-void RGB_LED(unsigned char led_color,unsigned char led_state);
-void Set_Pin_value(unsigned char port,unsigned char pin,unsigned char pin_value);
-void Toggle_pin(unsigned char port, unsigned char pin);
+unsigned char Get_Switch(u8 sw);
+void RGB_LED(u8 led_color,u8 led_state);
+void Set_Pin_value(u8 port,u8 pin,u8 pin_value);
+void Toggle_pin(u8 port, u8 pin);
---
+++ COTS/01_MCAL/01_GPIO/GPIO_Private.h
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<d:\Projects\C_projects\tm4c123gh6pm.h>
+
+// led color
+#define RED_LED 0
+#define BLUE_LED 1
+#define GREEN_LED 2
+// switches
+#define SW_1 0
+#define SW_2 1
+// led state
+#define LED_OFF 0
+#define LED_ON 1
+// Gpio ports
+#define portA (0)
+#define portB (1)
+#define portC (2)
+#define portD (3)
+#define portE (4)
+#define portF (5)
+//GPIO pins
+#define pin0 (1<<0)
+#define pin1 (1<<1)
+#define pin2 (1<<2)     
+#define pin3 (1<<3)
+#define pin4 (1<<4)
+#define pin5 (1<<5)
+#define pin6 (1<<6)
+#define pin7 (1<<7)
+//Gpio pin data
+#define Pin_High (1)
+#define Pin_Low  (0)
+//Gpio pin direction
+#define pin_out (1)
+#define pin_in  (0)
+// keys 
+#define key_lock 0x4c4f434B
+#define Enable_portF_pins 0x1F
+#define Enable_portA_Uart_Spi 0x3F
+#define PUL_R_PortF 0x11
+#define PUL_R_PortA 0x33
+#define Out_Leds 0x0E
+#define Out_TXs 0x22
+#define No_Alternate_func 0x00000000
+#define Uart_Spi_PortA 0x3F
