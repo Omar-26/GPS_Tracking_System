d2ab3c4c4fecd56d38847b055bdf0e6f61ba4a64
Omar Ashraf <eng.omar.ashraf26@gmail.com>
2024-05-08 01:46:58 +0300

    Updated Drivers

--- COTS/01_MCAL/05_EEPROM/EEPROM_Program.c
+++ COTS/01_MCAL/05_EEPROM/EEPROM_Program.c
@@ -1,7 +1,7 @@
-#include "eeprom.h"
+#include "EEPROM_Interface.h"
 #include "tm4c123gh6pm.h"
 
-//intialization of eeprom clk
+// intialization of eeprom clk
 
 Std_ReturnType init_EEPROM(void)
 {
@@ -9,19 +9,20 @@ Std_ReturnType init_EEPROM(void)
 
     //_delay_cycles(3);
 
-    while((SYSCTL_RCGCEEPROM_R & Activate_eeprom)==0);
+    while ((SYSCTL_RCGCEEPROM_R & Activate_eeprom) == 0)
+        ;
 
-    while((EEPROM_EEDONE_R & 0x01) == 1);
+    while ((EEPROM_EEDONE_R & 0x01) == 1)
+        ;
 
     if ((EEPROM_EESUPP_R & 0XC) == 1)
     {
-     return E_NOT_OK;
+        return E_NOT_OK;
     }
-    else 
+    else
     {
-     return E_OK;
+        return E_OK;
     }
-
 }
 
 void write_EEPROM(uint16_t address, uint32_t data)
@@ -33,17 +34,16 @@ void write_EEPROM(uint16_t address, uint32_t data)
 
     EEPROM_EERDWR_R = data;
 
-    while (EEPROM_EEDONE_R & EEPROM_EEDONE_WORKING);
-
+    while (EEPROM_EEDONE_R & EEPROM_EEDONE_WORKING)
+        ;
 }
 
 uint32_t read_EEPROM(uint16_t address)
 {
 
-EEPROM_EEBLOCK_R = address >> 4;
+    EEPROM_EEBLOCK_R = address >> 4;
 
-EEPROM_EEOFFSET_R = address & 0xF;
+    EEPROM_EEOFFSET_R = address & 0xF;
 
-return EEPROM_EERDWR_R;
-
+    return EEPROM_EERDWR_R;
 }
